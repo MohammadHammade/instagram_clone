1883827c915ba8dba17d057cb21d95f50ced4d42
MohammadHammade <medo.spider0000@gmail.com>
2021-09-20 01:32:15 +0300

    first commit

---
+++ resources/views/mywelcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="container text-center">
+<div class="row bg-transparent shadow-lg p-5">
+    <div class="col-6">
+            <img class=" rounded-circle" src="http://www.placehold.it/300/200"  alt="test">
+    </div>
+    <div class="col-6">Test2</div>
+</div>
+</div>
+
+@endsection
